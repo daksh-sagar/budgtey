7e1c18b9327dab55d10db6482bf82d182b14782f
Daksh <daksh.sagar@yahoo.com>
2018-12-24 12:35:46 +0530

    Get input data

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const budgetController = (function() {})();
 
-const uiController = (function() {})();
+const uiController = (function() {
+  return {
+    getInput: () => {
+      const type = document.querySelector('.add__type').value;
+      const description = document.querySelector('.add__description').value;
+      const value = document.querySelector('.add__value').value;
+
+      return {
+        type,
+        description,
+        value
+      };
+    }
+  };
+})();
 
 const controller = (function(budgetCtrl, uiCtrl) {
   const ctrlAddItem = () => {
     //TODO 1) get the input data
+    const inputData = uiCtrl.getInput();
     // pass the data to budget controller and ui controller
     // update the budget and then update the ui
-    console.log('ctrlAddItem called');
+    console.log(inputData);
   };
 
   document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
