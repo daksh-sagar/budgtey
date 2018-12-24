2ec9c93a077d4c0ac98e3a5e56f4f8df59845f15
Daksh <daksh.sagar@yahoo.com>
2018-12-24 12:05:14 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const budgetController = (function() {})();
+
+const uiController = (function() {})();
+
+const controller = (function(budgetCtrl, uiCtrl) {})(
+  budgetController,
+  uiController
+);
