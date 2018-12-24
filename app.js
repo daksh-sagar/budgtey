f4030e1a3b94d6447c00217fb6a0e8a7236c920b
Daksh <daksh.sagar@yahoo.com>
2018-12-24 12:18:56 +0530

    Add event listeners

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ const budgetController = (function() {})();
 
 const uiController = (function() {})();
 
-const controller = (function(budgetCtrl, uiCtrl) {})(
-  budgetController,
-  uiController
-);
+const controller = (function(budgetCtrl, uiCtrl) {
+  const ctrlAddItem = () => {
+    //TODO 1) get the input data
+    // pass the data to budget controller and ui controller
+    // update the budget and then update the ui
+    console.log('ctrlAddItem called');
+  };
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', event => {
+    if (event.keyCode === 13) {
+      ctrlAddItem();
+    }
+  });
+})(budgetController, uiController);
